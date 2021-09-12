fdc28912ba44a6e1d5008edb6a22af3f119287e9
MD Mofasser Hossain <60401072+mofasserrkb@users.noreply.github.com>
2021-09-12 21:06:25 +0600

    A few  changes made

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <span>Online News Website|© Copyright 2021  | Powered by MD.Mofasser Hossain</span>
+                <span>© Copyright 2021| News Powered by MD.Mofasser Hossain</span>
             </div>
         </div>
     </div>
