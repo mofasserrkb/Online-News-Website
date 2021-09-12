87208b731203e0f5bde6e15da8d02319e093a3d9
MD Mofasser Hossain <60401072+mofasserrkb@users.noreply.github.com>
2021-09-12 20:56:42 +0600

    file changed

--- admin/footer.php
+++ admin/footer.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <span> News |© Copyright 2021| Powered by MD.Mofasser Hossain</span>
+                <span>Online News Website|© Copyright 2021| Powered by MD.Mofasser Hossain</span>
             </div>
         </div>
     </div>
