8312549128c988fa31ec0308ec1116b2779f19b2
Mofasser <mofasserrakib@gmail.com>
2021-09-12 20:49:13 +0600

    File updated

--- admin/footer.php
+++ admin/footer.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <span>© Copyright 2021 News | Powered by MD.Mofasser Hossain</a></span>
+                <span>© Copyright 2021 News | Powered by MD.Mofasser Hossain</span>
             </div>
         </div>
     </div>
